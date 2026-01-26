bd9d99226d570acee2a4e74e8ff209fc7188db8b
MiloBytesCode <milo.zogbi@outlook.com>
2026-01-26 11:50:36 -0500

    Improved incorrect detection efficiency

--- src/code.cpp
+++ src/code.cpp
@@ -8,19 +8,20 @@
 #include <string>
 using namespace std;
 
-#include <ctime> 
 
 Code::Code(int n, int m)
-// Constructor : inits data members
+// Constructor, inits data members
 {
     this->n = n;
     this->m = m;
 }
 
+
 void Code::createSecretCode()
 // Generates a randomized secret code of length m with digits from 0 to n
 {
-    stored_code.clear(); //clear vector to prevent growth on multiple calls
+    // empty vector before generating code
+    stored_code.clear();
 
     //set seed for random number generator
     srand(time(0)); 
@@ -28,7 +29,6 @@ void Code::createSecretCode()
     for(int i = 0; i < n; i++)
     {
         stored_code.push_back(rand() % (n + 1));
-        //stored_code.push_back(rand() % m); 
     }
 }
 
@@ -42,7 +42,7 @@ int Code::checkCorrect(const Code& guess) const
 
     for (int i = 0; i < n; i++)
     {
-
+        // the same value at the same index is considered correct
         if (guess_code[i] == stored_code[i]) 
         {
             correct++;
@@ -55,88 +55,37 @@ int Code::checkCorrect(const Code& guess) const
 
 int Code::checkIncorrect(const Code& guess) const
 // Takes user guess and compares with secret_code, returns number of correct
-// digirts in incorrect location
+// digits at incorrect location (semi_correct), no double counting
 { 
     int semi_correct = 0;
     vector<int> guess_code = guess.getCode();
     vector<int> secret_code = stored_code;
-    
-    vector<bool> track_usedSecret(n, false); //track used digits in secret code
-    vector<bool> track_usedGuess(n, false); //track used digits in guess code
 
-    // first check all correct position matches
-    for(int i = 0; i < n; i++)
+    for (int i = 0; i < n; i++)   // iterate through GUESS
     {
-        if(stored_code[i] == guess_code[i])
+        for (int j = 0; j < n; j++)     // iterate through SECRET
         {
-            track_usedSecret[i] = true;
-            track_usedGuess[i] = true;
-        }
-    }
-    
-    for(int i = 0; i < n; i++)
-    {
-        if(track_usedGuess[i]) continue; // go on if already matched guess
-
-        for(int j = 0; j < n; j++)
-        {
-            if(track_usedSecret[j]) continue; //go onto next secret digit if already matched
-
-            // check for semi-correct match
-            if(guess_code[i] == stored_code[j])
+            if(guess_code[i] == secret_code[j] && (i != j))     // if semi_correct case is true
             {
+
+                if (guess_code[j] == secret_code[j]) { continue; }     // avoid double counting correct case 
+
+                // remove the pair of values from the search (-1 is inaccessible)
+                guess_code[i] = -1;
+                secret_code[i] = -1;
+
                 semi_correct++;
-                track_usedSecret[j] = true;
-                track_usedGuess[i] = true;
-                break;
-                
             }
         }
-    }
 
-   /* for (int i = 0; i < n; i++)
-    {
-
-        if (guess_code[i] == secret_code[i])
-        {
-            break;
-
-        } else
-        {
-            for (int j = 0; j < n; j++)
-            {
-                if(guess_code[i] == secret_code[j] && (i != j))
-                {
-                    // special case
-                    if (guess_code[j] == secret_code[j]) { break; }
-
-                    guess_code[i] = -1;
-                    semi_correct++;
-                }
-            }
-
-        }
-
-    }
-        */
+     }
 
     return semi_correct;
 }
 
 
-/*
-41122 -> secret
-
-52326 -> 1 0
-
-
-
-
-
-*/
-
 string Code::printCode() const
-// returns the secret code as a string for use in testing
+// utility function that returns the stored code as a string
 {
     string c = "";
     for (int i : stored_code)
@@ -146,6 +95,7 @@ string Code::printCode() const
     return c;
 }
 
+
 void Code::setCode(vector<int> new_code)
 // takes an int vector input and resets the stored_code data member
 {
@@ -154,7 +104,7 @@ void Code::setCode(vector<int> new_code)
 
 
 vector<int> Code::getCode() const
-// returns the code stored in the object
+// returns stored code data member
 {
     return stored_code;
 }
