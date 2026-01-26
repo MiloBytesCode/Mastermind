6ae86e1f7167366babbf1cef1204c21311d57e18
MiloBytesCode <milo.zogbi@outlook.com>
2026-01-26 09:18:29 -0500

    Placed all sources in src dir, compiled successfully

--- src/main.cpp
+++ src/main.cpp
@@ -69,3 +69,33 @@ int main()
 
 
 }
+
+
+
+
+
+
+/////// MAIN FOR PART B
+/*
+#include "mastermind.h"
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n, m;
+
+    cout << "Enter code length you want to guess: ";
+    cin >> n;
+ 
+
+    cout << "Enter how many should the digit range be(digits are 0 to m-1): ";
+    cin >> m;
+ 
+
+    Mastermind game(n, m);
+    game.playGame();
+    return 0;
+}
+
+*/
