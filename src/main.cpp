88fc6ffb3e111d704441f25d58fd71378951e67c
MiloBytesCode <milo.zogbi@outlook.com>
2026-01-26 11:51:19 -0500

    Cleaned main to remove incorrect/correct variables

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,12 @@
 /* Header */
 
 
-/* --- DEPENDENCIES --- */
+
 #include "code.h"
 #include <iostream>
 using namespace std;
 
+// PART A
 int main()
 {
     // test guesses
@@ -13,17 +14,8 @@ int main()
     vector<int> guess_2 = {2, 1, 2, 2, 2}; 
     vector<int> guess_3 = {1, 3, 3, 4, 5}; 
 
-    // number of digits (n) and length (m) test case
+    // n length, m digits
     int n = 5, m = 10;  
-    /*
-    cout << "please enter max number possible and number of digits" << endl;
-    cout << "Max number: ";
-    cin >> n;
-    n += 1;
-    cout << "\nnumber of digits: ";
-    cin >> m;
-    //code for taking in codemaker params, commented out because the original guess codes need specific values of n and m
-        */
     
     // init maker
     Code code_maker(n, m);
@@ -33,41 +25,33 @@ int main()
     Code code_breaker(n, m);
 
 
-    int correct, incorrect;
-
     // begin
     cout << "=====  Welcome to Mastermind!  ======" << endl;
     cout << "-> Secret Code = " << code_maker.printCode() << endl;
 
     // try guess 1
     code_breaker.setCode(guess_1);
-    correct = code_maker.checkCorrect(code_breaker);
-    incorrect = code_maker.checkIncorrect(code_breaker);
 
     cout << "Guess 1 [5,0,3,2,6] returns : ";
-    cout << "correct = " << correct << " | incorrect = " << incorrect << endl;
+    cout << "correct = " << code_maker.checkCorrect(code_breaker);;
+    cout << " | incorrect = " << code_maker.checkIncorrect(code_breaker) << endl;
 
     // try guess 2
     code_breaker.setCode(guess_2);
-    correct = code_maker.checkCorrect(code_breaker);
-    incorrect = code_maker.checkIncorrect(code_breaker);
 
     cout << "Guess 2 [2,1,2,2,2] returns : ";
-    cout << "correct = " << correct << " | incorrect = " << incorrect << endl;
+    cout << "correct = " << code_maker.checkCorrect(code_breaker);;
+    cout << " | incorrect = " << code_maker.checkIncorrect(code_breaker) << endl;
 
     // try guess 3
     code_breaker.setCode(guess_3);
-    correct = code_maker.checkCorrect(code_breaker);
-    incorrect = code_maker.checkIncorrect(code_breaker);
 
     cout << "Guess 3 [1,3,3,4,5] returns : ";
-    cout << "correct = " << correct << " | incorrect = " << incorrect << endl;
+    cout << "correct = " << code_maker.checkCorrect(code_breaker);;
+    cout << " | incorrect = " << code_maker.checkIncorrect(code_breaker) << endl;
 
 
-
-    return 0; //ends main function
-
-
+    return 0;
 }
 
 
