96879b87b05ff047531da4d7e32a9f4d7901c0d4
enderpyy <widderender@gmail.com>
2026-01-27 20:14:57 -0500

    commented out test method

--- src/main_parta.cpp
+++ src/main_parta.cpp
@@ -25,8 +25,9 @@ int main()
     Code code_maker(n, m);          // init MAKER
     code_maker.createSecretCode();
 
-    vector<int> test_code = {2, 5, 2, 2, 3};
-    code_maker.setCode(test_code);
+    //this code is for forcing a value to test, if needed
+    //vector<int> test_code = {2, 5, 2, 2, 3};
+    //code_maker.setCode(test_code);
 
     Code code_breaker(n, m);        // init BREAKER
 
